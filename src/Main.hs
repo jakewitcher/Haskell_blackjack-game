6941891211c6f2a17879ce7e3e648e2353010b9c
Jake Witcher <jjwitcher09@gmail.com>
2019-04-26 20:53:51 -0400

    change domain model

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
+import System.Random (randomRIO)  
+import GameTypes
+
 main :: IO ()
 main = do
   putStrLn "blackjack"
+
+ranks = [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]
+suits = [Spade, Club, Heart, Diamond]
+
+
+makeCards suits ranks =
+  foldr (\suit cards -> map (\rank -> go suit rank) ranks ++ cards) [] suits
+  where go s r =
+          Card s r
